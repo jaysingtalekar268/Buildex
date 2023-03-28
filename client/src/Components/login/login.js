0e76a99acfd1dd79e3d1a20046f572a610edd9dc
sdidd <77382851+sdidd@users.noreply.github.com>
2023-03-28 22:32:11 +0530

    Add files via upload

--- client/src/Components/login/login.js
+++ client/src/Components/login/login.js
@@ -1,13 +1,20 @@
 import { useNavigate } from 'react-router-dom'
 import React, { useState, useEffect } from 'react';
+import { Container, Button, Tabs, Tab } from 'react-bootstrap';
 
 function Login() {
 
     const [name, setName] = useState("");
-
     const [pwd, setPwd] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
+    const [role, setRole] = useState("");
+
+
     const [nameWarn, setNwarn] = useState("");
     const [passWarn, setPwarn] = useState("");
+
+    const [tabKey, setTab] = useState('login');
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -36,6 +43,35 @@ function Login() {
             localStorage.setItem("user", JSON.stringify(loginStatus));
             navigate("/");
         }
+       
+    };
+
+
+    const register = async () => {
+        let registerStatus = await fetch("http://localhost:3001/useradd", {
+            method: "post",
+            body: JSON.stringify({
+                "name": name,
+                "password": pwd,
+                "phone":phone,
+                "email":email,
+                "role":role
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            }
+
+        })
+
+        registerStatus = await registerStatus.json();
+        console.warn(registerStatus);
+
+        if (registerStatus.name) {
+            {
+                setTab("login");
+            // navigate("/");
+            }
+        }
     };
 
 
@@ -45,13 +81,13 @@ function Login() {
 
         let nPass = false;
         let pPass = false;
-        if (name.length === 0)
+        if (name.length == 0)
             setNwarn("Please enter Username");
         else {
             setNwarn("");
             nPass = true;
         }
-        if (pwd.length === 0)
+        if (pwd.length == 0)
             setPwarn("Please enter Password");
         else {
             setPwarn("");
@@ -59,22 +95,54 @@ function Login() {
         }
 
         if (nPass && pPass) {
+            if(tabKey=="login")
+            {
             login();
+            }
+            else if(tabKey=="register")
+            {
+                register();
+            }
+
         }
 
     };
 
 
     return (
-        <div class="card mx-auto container-md align-middle shadow p-3 mb-5 bg-body rounded">
-            <div class="card-body align-content-center text-center align-middle">
-                <input class='form-text' onChange={(e) => setName(e.target.value)} type="text" placeholder='Enter Username'></input>
-                <div class='text-warning'>{nameWarn}</div>
-                <input class='form-text' onChange={(e) => setPwd(e.target.value)} type="password" placeholder='Enter Password'></input>
-                <div class='text-warning align-items-md-end'>{passWarn}</div>
-                <div class='btn btn-primary bg-gradient align-middle align-self-center ' onClick={CheckInput}>Login</div>
-            </div>
-        </div>
+
+        <Container className='formContainer'>
+            <Tabs id="controlled-tab-example"
+                activeKey={tabKey}
+                onSelect={(k) => setTab(k)}
+                className="mb-3">
+                <Tab eventKey="login" >
+                    <input className='formInput' onChange={(e) => setName(e.target.value)} type="text" placeholder='Enter Username'></input>
+                    <span className='formWarn'>{nameWarn}</span>
+                    <input className='formInput' onChange={(e) => setPwd(e.target.value)} type="text" placeholder='Enter Password'></input>
+                    <span className='formWarn'>{passWarn}</span>
+                    <a className='formInput' onClick={() => setTab("register")}>New to Buildex?</a>
+                    <button onClick={CheckInput}>Login</button>
+
+                </Tab>
+                <Tab eventKey="register">
+                    <input className='formInput' onChange={(e) => setName(e.target.value)} type="text" placeholder='Enter Username'></input>
+                    <span className='formWarn'>{nameWarn}</span>
+                    <input className='formInput' onChange={(e) => setPwd(e.target.value)} type="text" placeholder='Enter Password'></input>
+                    <span className='formWarn'>{passWarn}</span>
+                    <input className='formInput' onChange={(e) => setEmail(e.target.value)} type="text" placeholder='Enter Email'></input>
+                    <span className='formWarn'>{passWarn}</span>
+                    <input className='formInput' onChange={(e) => setPhone(e.target.value)} type="text" placeholder='Enter Phone'></input>
+                    <span className='formWarn'>{passWarn}</span>
+                    <input className='formInput' onChange={(e) => setRole(e.target.value)} type="text" placeholder='Enter Role'></input>
+                    <span className='formWarn'>{passWarn}</span>
+                    <a className='formInput' onClick={() => setTab("login")}>Already have account</a>
+                    <button onClick={CheckInput}>Register</button>
+                </Tab>
+            </Tabs>
+
+        </Container>
+
     );
 
 };
--- server/db/user.js
+++ server/db/user.js
@@ -6,6 +6,12 @@ const userSchema = new mongoose.Schema({
     password:String,
     role:String,
     phone:String,
+    userevent:[
+        {
+        event_body: String,
+        
+        event_date :Date
+    }],
     project_id:[{
         type:mongoose.Schema.Types.ObjectId,
         ref:'projects'
