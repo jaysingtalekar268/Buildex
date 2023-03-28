1e3da58b6592bd785a6f45bba820bfe90ef56452
sdidd <sdiddtayade@gmail.com>
2023-03-29 00:59:58 +0530

    Direct main branch fuck it bro te branch change nahi hot ahe

--- client/src/Components/login/login.js
+++ client/src/Components/login/login.js
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 import React, { useState, useEffect } from 'react';
-import { Container, Button, Tabs, Tab } from 'react-bootstrap';
+import { Container, Button, Tabs, div } from 'react-bootstrap';
+import './login.css'
 
 function Login() {
 
@@ -43,7 +44,7 @@ function Login() {
             localStorage.setItem("user", JSON.stringify(loginStatus));
             navigate("/");
         }
-       
+
     };
 
 
@@ -53,9 +54,9 @@ function Login() {
             body: JSON.stringify({
                 "name": name,
                 "password": pwd,
-                "phone":phone,
-                "email":email,
-                "role":role
+                "phone": phone,
+                "email": email,
+                "role": role
             }),
             headers: {
                 'Content-type': 'application/json'
@@ -69,7 +70,7 @@ function Login() {
         if (registerStatus.name) {
             {
                 setTab("login");
-            // navigate("/");
+                // navigate("/");
             }
         }
     };
@@ -95,12 +96,10 @@ function Login() {
         }
 
         if (nPass && pPass) {
-            if(tabKey=="login")
-            {
-            login();
+            if (tabKey == "login") {
+                login();
             }
-            else if(tabKey=="register")
-            {
+            else if (tabKey == "register") {
                 register();
             }
 
@@ -111,37 +110,40 @@ function Login() {
 
     return (
 
-        <Container className='formContainer'>
-            <Tabs id="controlled-tab-example"
-                activeKey={tabKey}
-                onSelect={(k) => setTab(k)}
-                className="mb-3">
-                <Tab eventKey="login" >
-                    <input className='formInput' onChange={(e) => setName(e.target.value)} type="text" placeholder='Enter Username'></input>
-                    <span className='formWarn'>{nameWarn}</span>
-                    <input className='formInput' onChange={(e) => setPwd(e.target.value)} type="text" placeholder='Enter Password'></input>
-                    <span className='formWarn'>{passWarn}</span>
-                    <a className='formInput' onClick={() => setTab("register")}>New to Buildex?</a>
-                    <button onClick={CheckInput}>Login</button>
-
-                </Tab>
-                <Tab eventKey="register">
-                    <input className='formInput' onChange={(e) => setName(e.target.value)} type="text" placeholder='Enter Username'></input>
-                    <span className='formWarn'>{nameWarn}</span>
-                    <input className='formInput' onChange={(e) => setPwd(e.target.value)} type="text" placeholder='Enter Password'></input>
-                    <span className='formWarn'>{passWarn}</span>
-                    <input className='formInput' onChange={(e) => setEmail(e.target.value)} type="text" placeholder='Enter Email'></input>
-                    <span className='formWarn'>{passWarn}</span>
-                    <input className='formInput' onChange={(e) => setPhone(e.target.value)} type="text" placeholder='Enter Phone'></input>
-                    <span className='formWarn'>{passWarn}</span>
-                    <input className='formInput' onChange={(e) => setRole(e.target.value)} type="text" placeholder='Enter Role'></input>
-                    <span className='formWarn'>{passWarn}</span>
-                    <a className='formInput' onClick={() => setTab("login")}>Already have account</a>
-                    <button onClick={CheckInput}>Register</button>
-                </Tab>
+        <div class='text-center position-absolute top-50 start-50 translate-middle shadow p-3 mb-5 bg-body rounded bg-info'>
+            <Tabs id="controlled-tab-example" activeKey={tabKey} onSelect={(k) => setTab(k)} class=''>
+                <div eventKey="login" class='card shadow text-center vw-50 vh-50'>
+                    <label>Your Username</label>
+                    <input class='form-text' onChange={(e) => setName(e.target.value)} type="text" placeholder='Enter Username'></input>
+                    <div class='text-warning'>{nameWarn}</div>
+                    <label>Your Password</label>
+                    <input class='form-text' onChange={(e) => setPwd(e.target.value)} type="password" placeholder='Enter Password'></input>
+                    <div class='text-warning align-items-md-end'>{passWarn}</div>
+                    <div><a class='link' onClick={() => setTab("register")}>New to Buildex?</a></div>
+                    <div class='btn btn-light btn-lg align-middle align-self-center ' onClick={CheckInput}>Login</div>
+                </div>
+                <div eventKey="register" class='card text-center shadow vw-50 vh-50'>
+                    <label>Your Username</label>
+                    <input className='form-text' onChange={(e) => setName(e.target.value)} type="text" placeholder='Enter Username'></input>
+                    <div class='text-warning align-items-md-end'>{nameWarn}</div>
+                    <label>Your Password</label>
+                    <input className='form-text' onChange={(e) => setPwd(e.target.value)} type="text" placeholder='Enter Password'></input>
+                    <div class='text-warning align-items-md-end'>{passWarn}</div>
+                    <label>Your Email</label>
+                    <input className='form-text' onChange={(e) => setEmail(e.target.value)} type="text" placeholder='Enter Email'></input>
+                    <div class='text-warning align-items-md-end'>{passWarn}</div>
+                    <label>Your Phone</label>
+                    <input className='form-text' onChange={(e) => setPhone(e.target.value)} type="text" placeholder='Enter Phone'></input>
+                    <div class='text-warning align-items-md-end'>{passWarn}</div>
+                    <label>Your Role</label>
+                    <input className='form-text' onChange={(e) => setRole(e.target.value)} type="text" placeholder='Enter Role'></input>
+                    <div class='text-warning align-items-md-end'>{passWarn}</div>
+                    <div><a class='link' onClick={() => setTab("login")}>Already have account</a></div>
+                    <button div class='btn btn-light btn-lg align-middle align-self-center ' onClick={CheckInput}>Register</button>
+                </div>
             </Tabs>
 
-        </Container>
+        </div>
 
     );
 
--- client/src/Components/navbar/navbar.js
+++ client/src/Components/navbar/navbar.js
@@ -11,16 +11,15 @@ const Navbar = () => {
 
 
     return (
-        <div className="navbar">
-            <div class="tab-content">
+        <div class="navbar navbar-light bg-light">
+            <div class="">
+                <Link class='nav-item'><img src='/home/sdidd/Desktop/FSDProject/Buildex/client/public/logo512.jpg'></img></Link>
                 <Link className="btn" to="/">Home</Link>
                 <Link className="btn" to="/MyProject">MyProject</Link>
                 <Link className="btn" to="/Message">Message</Link>
                 <Link className="btn" to="/Profile">Profile</Link>
                 {auth ? <Link className="btn" to='/Login' onClick={() => localStorage.clear()}>Logout</Link> : <Link className="btn" to='/Login' >Login</Link>}
             </div>
-
-
         </div>
     )
 }
