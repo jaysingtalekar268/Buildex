5fcd3b37ecb9adc010f01cd322367659a2140a78
sdidd <sdiddtayade@gmail.com>
2023-03-28 20:05:26 +0530

    merge this in main i will then fetch main

--- client/src/Components/login/login.js
+++ client/src/Components/login/login.js
@@ -1,7 +1,5 @@
-import './login.css';
 import { useNavigate } from 'react-router-dom'
 import React, { useState, useEffect } from 'react';
-import { Container,Button } from 'react-bootstrap';
 
 function Login() {
 
@@ -11,19 +9,19 @@ function Login() {
     const [nameWarn, setNwarn] = useState("");
     const [passWarn, setPwarn] = useState("");
     const navigate = useNavigate();
-    
+
     useEffect(() => {
-      const  auth=localStorage.getItem("user") ;
+        const auth = localStorage.getItem("user");
         if (auth) {
             navigate("/");
         }
     });
-    const login =async ()=>{
-        let loginStatus=await fetch("http://localhost:3001/Login",{
-            method:"post",
+    const login = async () => {
+        let loginStatus = await fetch("http://localhost:3001/Login", {
+            method: "post",
             body: JSON.stringify({
-                "name":name,
-                "password":pwd
+                "name": name,
+                "password": pwd
             }),
             headers: {
                 'Content-type': 'application/json'
@@ -31,54 +29,52 @@ function Login() {
 
         })
 
-        loginStatus= await loginStatus.json();
+        loginStatus = await loginStatus.json();
         console.warn(loginStatus);
 
-        if(loginStatus.name)
-        {
-            localStorage.setItem("user",JSON.stringify(loginStatus));
+        if (loginStatus.name) {
+            localStorage.setItem("user", JSON.stringify(loginStatus));
             navigate("/");
         }
     };
 
-    
+
 
     const CheckInput = () => {
         // alert('You clicked me!');
 
-        let nPass=false;
-        let pPass=false;
-        if(name.length==0)
-        setNwarn("Please enter Username");
-        else{
+        let nPass = false;
+        let pPass = false;
+        if (name.length === 0)
+            setNwarn("Please enter Username");
+        else {
             setNwarn("");
-            nPass=true;
+            nPass = true;
         }
-        if(pwd.length==0)
-        setPwarn("Please enter Password");
-        else{
+        if (pwd.length === 0)
+            setPwarn("Please enter Password");
+        else {
             setPwarn("");
-            pPass=true;
+            pPass = true;
         }
 
-        if(nPass && pPass)
-        {
+        if (nPass && pPass) {
             login();
         }
 
     };
-   
-     
+
+
     return (
-        
-        <Container className='formContainer'>
-            <input className='formInput' onChange={(e) => setName(e.target.value)} type="text" placeholder='Enter Username'></input>
-            <span className='formWarn'>{nameWarn}</span>
-            <input className='formInput' onChange={(e) => setPwd(e.target.value)}  type="text" placeholder='Enter Username'></input>
-            <span className='formWarn'>{passWarn}</span>
-            <button onClick={CheckInput}>Login</button>;
-        </Container>
-        
+        <div class="card mx-auto container-md align-middle shadow p-3 mb-5 bg-body rounded">
+            <div class="card-body align-content-center text-center align-middle">
+                <input class='form-text' onChange={(e) => setName(e.target.value)} type="text" placeholder='Enter Username'></input>
+                <div class='text-warning'>{nameWarn}</div>
+                <input class='form-text' onChange={(e) => setPwd(e.target.value)} type="password" placeholder='Enter Password'></input>
+                <div class='text-warning align-items-md-end'>{passWarn}</div>
+                <div class='btn btn-primary bg-gradient align-middle align-self-center ' onClick={CheckInput}>Login</div>
+            </div>
+        </div>
     );
 
 };
